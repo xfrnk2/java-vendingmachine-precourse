5e3829a97b0028c7ce1a53dbf0ce43c6249d2458
xfrnk2 <xfrnk2@gmail.com>
2021-12-09 18:37:22 +0900

    test: 상품 목록 및 상품 등록시 테스트 성공

--- src/test/java/vendingmachine/VendingMachineTest.java
+++ src/test/java/vendingmachine/VendingMachineTest.java
@@ -4,10 +4,14 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import static camp.nextstep.edu.missionutils.test.Assertions.*;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import vendingmachine.change.Change;
 import vendingmachine.change.ChangeAmount;
+import vendingmachine.item.Item;
+import vendingmachine.item.Items;
 
 public class VendingMachineTest {
 
@@ -26,8 +30,29 @@ public class VendingMachineTest {
 		Assertions.assertEquals(changes.get(100), 4);
 		Assertions.assertEquals(changes.get(50), 1);
 		Assertions.assertEquals(changes.get(10), 0);
-
-
 	}
 
+	@Test
+	public void initializeItems(){
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			Items items = new Items(Arrays.asList(";"));
+		});
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			Items items = new Items(Arrays.asList(","));
+		});
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			Items items = new Items(Arrays.asList(""));
+		});
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			Items items = new Items(Arrays.asList("a,b"));
+		});
+	}
+
+	@Test
+	public void initializeItem(){
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			Item item = new Item("[콜라,-1,101]");
+			Item item2 = new Item("[콜라,asd,]");
+		});
+	}
 }
