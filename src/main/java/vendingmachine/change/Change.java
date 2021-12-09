0bad8f13d5f1beef3b489a98d320db94f0df4d07
xfrnk2 <xfrnk2@gmail.com>
2021-12-09 17:55:31 +0900

    feat(Change): 음수 검사 로직 추가

--- src/main/java/vendingmachine/change/Change.java
+++ src/main/java/vendingmachine/change/Change.java
@@ -6,14 +6,22 @@ public class Change {
 	private final int money;
 
 	public Change(String money) {
-		this.money = isNumber(money);
+		this.money = convertToNumber(money);
+		validate(this.money);
+
 	}
 
-	private int isNumber(String money) {
+	private int convertToNumber(String money) {
 		try {
 			return Integer.parseInt(money);
 		} catch (NumberFormatException e) {
-			throw new IllegalArgumentException(TextType.ERROR_IS_NOT_NUMBER.getError());
+			throw new IllegalArgumentException(String.format(TextType.ERROR_IS_NOT_NUMBER.getError(), "자판기 보유 금액"));
+		}
+	}
+
+	private void validate(int money){
+		if (money < 0){
+			throw new IllegalArgumentException(TextType.ERROR_IS_NEGATIVE.getError());
 		}
 	}
 
