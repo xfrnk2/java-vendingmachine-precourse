cff29e8a8f16e57110c24f4696a8e5de3a5e795f
xfrnk2 <xfrnk2@gmail.com>
2021-12-09 18:36:42 +0900

    refactor: Item
    - 컨벤션 준수
    - 상품의 가격, 수량에 0 이하 유효성 검사 로직 추가
    - 숫자임을 검사하는 메소드의 파라메터 명을 money -> value로 변경

--- src/main/java/vendingmachine/item/Item.java
+++ src/main/java/vendingmachine/item/Item.java
@@ -7,15 +7,13 @@ import com.google.common.base.Splitter;
 import vendingmachine.type.DelimiterType;
 import vendingmachine.type.TextType;
 
-
 public class Item {
 
 	private final String name;
 	private final int cost;
 	private final int amount;
 
-
-	Item(String itemDetail){
+	public Item(String itemDetail) {
 
 		List<String> itemInfoList = initialize(itemDetail);
 		this.name = itemInfoList.get(0);
@@ -23,46 +21,54 @@ public class Item {
 		this.amount = convertToNumber(itemInfoList.get(2), "수량");
 	}
 
-	public List<String> initialize(String itemDetail) throws IllegalArgumentException{
-			List<String> itemInfoList = Splitter.on(DelimiterType.SEMICOLON.getDelimiter())
-				.omitEmptyStrings()
-				.trimResults()
-				.splitToList(extractPrefixAndSuffix(itemDetail));
-			validate(itemInfoList);
-			return itemInfoList;
+	public List<String> initialize(String itemDetail) {
+		List<String> itemInfoList = Splitter.on(DelimiterType.COMMA.getDelimiter())
+			.omitEmptyStrings()
+			.trimResults()
+			.splitToList(extractPrefixAndSuffix(itemDetail));
+		System.out.println(itemInfoList.size());
+		validateSize(itemInfoList);
+
+		return itemInfoList;
 	}
 
-	private String extractPrefixAndSuffix(String itemDetail){
+	private String extractPrefixAndSuffix(String itemDetail) {
 		if (!(itemDetail.startsWith(DelimiterType.ITEM_PREFIX.getDelimiter())
-			&& itemDetail.endsWith(DelimiterType.ITEM_SUFFIX.getDelimiter()))){
+			&& itemDetail.endsWith(DelimiterType.ITEM_SUFFIX.getDelimiter()))) {
 			throw new IllegalArgumentException("상품 상세는 여는 대괄호([)로 시작하고, 닫는 대괄호(])로 끝나야 합니다.");
 		}
-		return itemDetail.substring(1, itemDetail.length()-1);
+		System.out.println(itemDetail.substring(1, itemDetail.length() - 1));
+		return itemDetail.substring(1, itemDetail.length() - 1);
 	}
 
-	private void validate(List<String> itemInfo){
-		if (itemInfo.size() != 3){
+	private void validateSize(List<String> itemInfo) {
+		if (itemInfo.size() != 3) {
 			throw new IllegalArgumentException("입력이 '상품명, 가격 ,수량' 세 항목 단위가 아닙니다.");
 		}
 	}
 
-	private int convertToNumber(String money, String unit) {
+	private int convertToNumber(String value, String unit) {
 		try {
-			return Integer.parseInt(money);
+			int converted = Integer.parseInt(value);
+			if (converted < 0) {
+				throw new IllegalArgumentException(TextType.ERROR_IS_NOT_NUMBER.getError());
+			}
+			return converted;
 		} catch (NumberFormatException e) {
 			throw new IllegalArgumentException(String.format(TextType.ERROR_IS_NOT_NUMBER.getError(), unit));
 		}
 	}
 
-	public String getName(){
+	public String getName() {
 		return this.name;
 	}
 
-	public int getCost(){
+	public int getCost() {
 		return this.cost;
 	}
 
-	public int getAmount(){
+	public int getAmount() {
 		return this.amount;
 	}
+
 }
