e0429f9e157130cca711fcf1e0f4f7e77f5ea8a7
xfrnk2 <xfrnk2@gmail.com>
2021-12-09 23:46:16 +0900

    refactor: 올바른 포맷의 view를 출력할 수 있도록 일부 문자 및 개행 수

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 import vendingmachine.type.TextType;
 
 public class InputView {
-	public static final String MACHINE_HOLDING_MONEY_MESSAGE = "자판기가 보유하고 있는 금액을 입력해 주세요.%n";
+	public static final String MACHINE_HOLDING_MONEY_MESSAGE = "자판기가 보유하고 있는 금액을 입력해 주세요.";
 	public static final String ITEM_LIST_MESSAGE = "상품명과 가격, 수량을 입력해 주세요.";
 	public static final String PAYMENT_AMOUNT_MESSAGE = "투입 금액을 입력해 주세요.";
 	public static final String ITEM_NAME_TO_BUY_MESSAGE = "구매할 상품명을 입력해 주세요.";
@@ -27,7 +27,8 @@ public class InputView {
 
 
 	public static String getItemNameToBuy() {
-		System.out.printf("%n" + ITEM_NAME_TO_BUY_MESSAGE + "%n");
+		System.out.printf(ITEM_NAME_TO_BUY_MESSAGE + "%n");
 		return Console.readLine();
 	}
+
 }
--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -3,9 +3,10 @@ package vendingmachine.view;
 import vendingmachine.type.TextType;
 
 public class OutputView {
-	private static final String REMAINING_PAYMENT_AMOUNT = "투입 금액 : %s원%n";
+	private static final String REMAINING_PAYMENT_AMOUNT = "투입 금액: %s원%n";
 	private static final String CHANGE = "잔돈";
-	private static final String CHANGE_COIN_QUANTITY = "%s - %s개%n";
+	private static final String CHANGE_COIN_QUANTITY = "%s원 - %s개%n";
+	private static final String MACHINE_HOLDING_CHANGE = "자판기가 보유한 동전";
 
 
 	public static void printError(String error, Object ...args){
@@ -22,5 +23,13 @@ public class OutputView {
 
 	public static void printChangeResult(){
 		System.out.println(CHANGE);
+	}
+
+	public static void printHoldingChanges(){
+		System.out.println(MACHINE_HOLDING_CHANGE);
+	}
+
+	public static void printNewLine(){
+		System.out.println();
 	}
 }
