68ec52ef0e9c33ae095f38fee1b4b8491459471c
xfrnk2 <xfrnk2@gmail.com>
2021-12-09 18:08:58 +0900

    feat: InputView 기능 추가
    - 상품 목록을 받아올 때 출력하는 텍스트

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -4,9 +4,15 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 	public static final String MACHINE_HOLDING_MONEY_MESSAGE = "자판기가 보유하고 있는 금액을 입력해 주세요.";
+	public static final String ITEM_LIST_MESSAGE = "상품명과 가격, 수량을 입력해 주세요.";
 
 	public static String getHoldingTotalChanges() {
 		System.out.println(MACHINE_HOLDING_MONEY_MESSAGE + "%n");
+		return Console.readLine();
+	}
+
+	public static String getItemList() {
+		System.out.println(ITEM_LIST_MESSAGE + "%n");
 		return Console.readLine();
 	}
 }
